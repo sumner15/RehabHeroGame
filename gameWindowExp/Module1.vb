cb552608fc95b2044cbff5ecf18591d9647c5e41
uci_Justin <uci_Justin@localhost>
2013-03-08 12:06:42 -0800

    I changed the hgignore file so that it would not ignore the dlls that we need in the bin folder. I also added a function to module 1 that will create any missing directories and files. This is necessary because we don't want hg to track all of our data files, but hg will not track empty folders. This version was tested using the version of the FINGER robot at uci and it worked properly.

--- gameWindowExp/Module1.vb
+++ gameWindowExp/Module1.vb
@@ -18,10 +18,53 @@ Module Module1
     Sub Main()
         GAMEPATH = GAMEPATH.Substring(0, GAMEPATH.LastIndexOf("\"))
         GAMEPATH = GAMEPATH.Substring(0, GAMEPATH.LastIndexOf("\") + 1)
+        makeAbsentDirectories()
 
         Application.EnableVisualStyles()
         menu = New Menu
         Application.Run(menu)
+
+    End Sub
+
+    ''------------------------------- make absent directories ---------------------------''
+    '' HG will not track empty directories, and we don't want it to track folders full of
+    '' data. As such, we need to make our data directories if they do not exist
+    Sub makeAbsentDirectories()
+        If (Not System.IO.Directory.Exists(GAMEPATH & "dataFiles")) Then
+            System.IO.Directory.CreateDirectory(GAMEPATH & "dataFiles")
+        End If
+
+        If (Not System.IO.Directory.Exists(GAMEPATH & "gainFiles")) Then
+            System.IO.Directory.CreateDirectory(GAMEPATH & "gainFiles")
+        End If
+
+        If (Not System.IO.Directory.Exists(GAMEPATH & "hitTimeFiles")) Then
+            System.IO.Directory.CreateDirectory(GAMEPATH & "hitTimeFiles")
+        End If
+
+        If (Not System.IO.Directory.Exists(GAMEPATH & "scoreFiles")) Then
+            System.IO.Directory.CreateDirectory(GAMEPATH & "scoreFiles")
+        End If
+
+        If (Not System.IO.Directory.Exists(GAMEPATH & "Subjects")) Then
+            System.IO.Directory.CreateDirectory(GAMEPATH & "Subjects")
+            Console.WriteLine("made subjects dir")
+        End If
+
+        If (Not System.IO.File.Exists(GAMEPATH & "Subjects\" & "default.txt")) Then
+            Dim defaultSubjectFile As New System.IO.StreamWriter(GAMEPATH & "Subjects\" & "default.txt")
+            defaultSubjectFile.WriteLine("0")
+            defaultSubjectFile.WriteLine("default")
+            defaultSubjectFile.WriteLine("9/9/2011 12:00:00 AM")
+            defaultSubjectFile.Close()
+            Console.WriteLine("made subjects file")
+        End If
+
+        If (Not System.IO.File.Exists(GAMEPATH & "Subjects\" & "allSubjects.txt")) Then
+            Dim allSubjectFile As New System.IO.StreamWriter(GAMEPATH & "Subjects\" & "allSubjects.txt")
+            allSubjectFile.WriteLine("default")
+            allSubjectFile.Close()
+        End If
 
     End Sub
 
