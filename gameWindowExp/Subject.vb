006c95f7e65d717cea1b3a1d6c782de7c6899500
jezhill <jezhill@localhost>
2013-03-25 23:29:45 -0400

    Overhauled the way things are save/loaded in subject-specific config files Added lastSessionDate and lastSessionNumber, and associated logic, to Subject

--- gameWindowExp/Subject.vb
+++ gameWindowExp/Subject.vb
@@ -6,19 +6,22 @@
 Public Class Subject
     Public num As Integer           ' number corresponding to subject
     Public ID As String             ' the user's subject ID
-    Public LoginDate As Date        ' date subject was created
+    Public LoginDate As DateTime        ' date subject was created
     Public Kp1 As Single
     Public Kp2 As Single
     Public Kd1 As Single
     Public Kd2 As Single
-    Public hand As String
     Public trial As Single
+    Public lastSessionDate As DateTime
+    Public lastSessionNumber As Integer
+
+    Private epoch As DateTime = "1970-01-01 00:00:00"
     'Public dataFiles(10) As String  'array of file names for experimental dta files
 
     '--------------------------------------------------------------------------------'
     '------------------------- constructor for new subjects -------------------------'
     '--------------------------------------------------------------------------------'
-    Public Sub New(ByRef subNum As Integer, ByRef subID As String, ByRef subHand As String, ByRef subTrial As Single)
+    Public Sub New(ByRef subNum As Integer, ByRef subID As String, ByRef subTrial As Single)
         num = subNum
         ID = subID
         LoginDate = Now
@@ -26,8 +29,9 @@ Public Class Subject
         Kp2 = 10
         Kd1 = 1
         Kd2 = 1
-        hand = subHand
         trial = subTrial
+        lastSessionDate = epoch
+        lastSessionNumber = 0
         writeSubjectFile()
     End Sub
 
@@ -39,18 +43,17 @@ Public Class Subject
     '----------------------------- write subject file -------------------------------'
     '--------------------------------------------------------------------------------'
     Private Sub writeSubjectFile()
-        Dim subjectFile As StreamWriter
-        subjectFile = New StreamWriter(GAMEPATH & "Subjects\" & ID & ".txt")
-        'subjectFile = My.Computer.FileSystem.OpenTextFileWriter(GAMEPATH & "Subjects\" & ID & ".txt", True)
-        subjectFile.WriteLine(num)
-        subjectFile.WriteLine(ID)
-        subjectFile.WriteLine(LoginDate)
-        subjectFile.WriteLine(Kp1)
-        subjectFile.WriteLine(Kp2)
-        subjectFile.WriteLine(Kd1)
-        subjectFile.WriteLine(Kd2)
-        subjectFile.WriteLine(hand)
-        subjectFile.WriteLine(trial)
+        Dim subjectFile As StreamWriter = New StreamWriter(GAMEPATH & "Subjects\" & ID & ".txt")
+        subjectFile.WriteLine("num: " & num)
+        subjectFile.WriteLine("ID: " & ID)
+        subjectFile.WriteLine("LoginDate: " & LoginDate.ToString("s").Replace("T", " "))
+        subjectFile.WriteLine("Kp1: " & Kp1)
+        subjectFile.WriteLine("Kp2: " & Kp2)
+        subjectFile.WriteLine("Kd1: " & Kd1)
+        subjectFile.WriteLine("Kd2: " & Kd2)
+        subjectFile.WriteLine("trial: " & trial)
+        subjectFile.WriteLine("lastSessionDate: " & lastSessionDate.ToString("s").Replace("T", " "))
+        subjectFile.WriteLine("lastSessionNumber: " & lastSessionNumber)
         subjectFile.Close()
     End Sub
 
@@ -58,19 +61,17 @@ Public Class Subject
     '------------------------------- write read file --------------------------------'
     '--------------------------------------------------------------------------------'
     Private Sub readSubjectFile(ByVal subId As String)
-        Dim subjectFile As StreamReader
-        subjectFile = New StreamReader(GAMEPATH & "subjects\" & subId & ".txt")
-        'subjectFile = My.Computer.FileSystem.OpenTextFileReader(GAMEPATH & "subjects\" & subId & ".txt")
-        num = subjectFile.ReadLine()
-        ID = subjectFile.ReadLine()
-        LoginDate = subjectFile.ReadLine()
-        Kp1 = subjectFile.ReadLine()
-        Kp2 = subjectFile.ReadLine()
-        Kd1 = subjectFile.ReadLine()
-        Kd2 = subjectFile.ReadLine()
-        hand = subjectFile.ReadLine()
-        trial = subjectFile.ReadLine()        
-        subjectFile.Close()
+        Dim fromFile As FileDict = New FileDict(GAMEPATH & "Subjects\" & subId & ".txt")
+        num = fromFile.Lookup("num", "0")
+        ID = fromFile.Lookup("ID", subId)
+        LoginDate = fromFile.Lookup("LoginDate", Now.ToString("s").Replace("T", " "))
+        Kp1 = fromFile.Lookup("Kp1", "10")
+        Kp2 = fromFile.Lookup("Kp2", "10")
+        Kd1 = fromFile.Lookup("Kd1", "1")
+        Kd2 = fromFile.Lookup("Kd2", "1")
+        trial = fromFile.Lookup("trial", "0")
+        lastSessionDate = fromFile.Lookup("lastSessionDate", epoch)
+        lastSessionNumber = fromFile.Lookup("lastSessionNumber", 0)
     End Sub
     '--------------------------------------------------------------------------------'
     '------------------------------- update subject file ----------------------------'
@@ -82,7 +83,51 @@ Public Class Subject
     '--------------------------------------------------------------------------------'
     '------------------------------- make trial var public --------------------------'
     '--------------------------------------------------------------------------------'
-    Public Function getTrial() As Single
-        Return trial
+    Public Function getSessionString() As String
+        Dim today As Boolean = Math.Floor(lastSessionDate.Subtract(epoch).TotalDays) = Math.Floor(Now.Subtract(epoch).TotalDays)
+        Dim never As Boolean = (lastSessionDate = epoch)
+        If never Then Return "no previous session"
+        Dim result As String = "Last measurement: session " & lastSessionNumber.ToString("000.##")
+        result = result & If(today, " today", " on " & lastSessionDate.ToString("yyyy-MM-dd"))
+        result = result & " at " & lastSessionDate.ToString("h:mm tt")
+        Return result
     End Function
+    Public Function getExpectedSessionNumber() As String
+        Dim hrs As Double = Now.Subtract(lastSessionDate).TotalHours
+        Dim number As Integer = If(hrs < 4, lastSessionNumber, lastSessionNumber + 1)
+        Return number.ToString("000.##")
+    End Function
+End Class
+
+Public Class FileDict
+    Private sourceFileName As String
+    Private content As Dictionary(Of String, String)
+
+    Public Sub New(fileName As String)
+        sourceFileName = fileName
+        Read()
+    End Sub
+    Public Sub Read()
+        Dim file As StreamReader = New StreamReader(sourceFileName)
+        Dim lines As String() = file.ReadToEnd().Split(vbNewLine)
+        file.Close()
+        content = New Dictionary(Of String, String)
+        Dim parts As String()
+        For Each line In lines
+            parts = line.Split({":"c}, 2)
+            If parts.Length = 2 Then content.Add(parts(0).Trim(), parts(1).Trim())
+        Next
+    End Sub
+    Public Function Lookup(key As String, defaultValue As String) As String
+        If content.ContainsKey(key) Then Return content(key) Else Return defaultValue
+    End Function
+    Public Sub Write()
+        Dim file As StreamWriter = New StreamWriter(sourceFileName)
+        Dim list As New List(Of String)(content.Keys)
+        For Each key In list
+            file.WriteLine("{0}: {1}", key, content(key))
+        Next
+        file.Close()
+    End Sub
+
 End Class
